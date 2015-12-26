d862753eed3dc626c537b743f0d095a9040f979e
Sergey Burmisov <skypath@gmail.com>
2015-12-26 20:57:31 +0300

    prevent cache

--- index.js
+++ index.js
@@ -13,6 +13,15 @@ app.use(iisBaseUrl());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 
+app.use(function (req, res, next) {
+  res.set({
+    'Cache-Control': 'no-cache, no-store, must-revalidate',
+    'Pragma': 'no-cache',
+    'Expires': 0,
+  });
+  next();
+});
+
 app.get('/', function (req, res) {
   return res.send('Feature-server v' + options.version);
 });
